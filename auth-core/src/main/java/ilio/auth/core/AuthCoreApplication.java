49f3cc8438a8d7b8870a34313b2116de15f3b10f
grsky360 <zs360@qq.com>
2019-04-04 19:09:56 +0800

    update

--- auth-core/src/main/java/ilio/auth/core/AuthCoreApplication.java
+++ auth-core/src/main/java/ilio/auth/core/AuthCoreApplication.java
@@ -1,11 +1,9 @@
 package ilio.auth.core;
 
-import ilio.auth.support.EnableAuth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAuth
 public class AuthCoreApplication {
 
     public static void main(String[] args) {
